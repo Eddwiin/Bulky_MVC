fa8030a1a73e4aea64d62b0274ed370cd8cebc36
eddwiin <maguran.mahendrarajan@gmail.com>
2023-11-20 16:51:02 +0100

    add CRUD for Category

--- Bulky_MVC/Controllers/CategoryController.cs
+++ Bulky_MVC/Controllers/CategoryController.cs
@@ -17,5 +17,51 @@ namespace Bulky_MVC.Controllers
 
             return View(objCategories);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+            }
+   
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null ||  id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null) {
+                return NotFound();
+            }
+
+
+            return View();
+        }
+
+        [HttpPut]
+        public IActionResult Edit(Category obj)
+        {
+            return View(obj);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
     }
 }
